9d21812816cfef1d1a9928ae2a5f6fd768289813
acharles21 <64337918+acharles21@users.noreply.github.com>
2026-09-19 07:16:37 -0700

    Add files via upload

---
+++ Science-By-Hugs-main/admin-sw.js
@@ -0,0 +1,44 @@
+const CACHE_NAME = 'sbh-admin-v2';
+
+self.addEventListener('install', event => {
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(
+        keys
+          .filter(key => key !== CACHE_NAME)
+          .map(key => caches.delete(key))
+      );
+    }).then(() => {
+      return self.clients.claim();
+    })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    fetch(event.request)
+      .then(response => {
+
+        if (
+          event.request.method === 'GET' &&
+          response &&
+          response.status === 200
+        ) {
+          const copy = response.clone();
+
+          caches.open(CACHE_NAME).then(cache => {
+            cache.put(event.request, copy);
+          });
+        }
+
+        return response;
+      })
+      .catch(() => {
+        return caches.match(event.request);
+      })
+  );
+});
